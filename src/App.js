8fc618a692b67b1d7e1e3c623d626fcb465da163
memelopez <meme30lopez@gmail.com>
2025-09-09 16:03:27 -0600

    Add header

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div className="hola">
-        <h2>hola mundo</h2>
+      <div className="header">
+        <h1>Jamming</h1>
       </div>
     </div>
   );
