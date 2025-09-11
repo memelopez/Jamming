388eaa9f5bf07ec537b3525ff146a641ca93ad0f
memelopez <meme30lopez@gmail.com>
2025-09-11 16:31:11 -0600

    Add most styles for search results area

--- src/components/searchResults/SearchResults.js
+++ src/components/searchResults/SearchResults.js
@@ -4,8 +4,17 @@ import styles from './searchResults.module.css';
 const SearchResults = () => {
   return (
     <div className={styles.searchResultsContainer}>
-      <div className={styles.forPlaylist}></div>
-      <div className={styles.forTracklist}></div>
+      <div className={styles.forPlaylist}>
+        <h2>Results</h2>
+      </div>
+      <div className={styles.forTracklist}>
+        <form className={styles.savePlaylistForm}>
+          <input type="text" name="playlistNameInput" id="playlistNameInput" className={styles.playlistNameInput} placeholder='Playlist Name'/>
+          <div className={styles.save2playlist}>
+          </div>
+          <input type="submit" value="SAVE TO SPOTIFY" className={styles.savePlaylistBtn}/>
+        </form>
+      </div>
     </div>
   );
 }
